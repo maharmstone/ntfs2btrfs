0d95d798d6b4e9f18f2759660ff4e9f3b3a11a53
Mark Harmstone <mark@harmstone.com>
2021-09-17 19:52:04 +0100

    avoid zeroing buffer_t on initialization

--- src/ntfs2btrfs.h
+++ src/ntfs2btrfs.h
@@ -88,7 +88,30 @@ struct data_alloc {
     bool not_in_img;
 };
 
-using buffer_t = std::vector<uint8_t>;
+template<typename T, typename A = std::allocator<T>>
+class default_init_allocator : public A {
+public:
+    typedef std::allocator_traits<A> a_t;
+
+    template<typename U>
+    struct rebind {
+        using other = default_init_allocator<U, typename a_t::template rebind_alloc<U>>;
+    };
+
+    using A::A;
+
+    template<typename U>
+    void construct(U* ptr) noexcept(std::is_nothrow_default_constructible<U>::value) {
+        ::new(static_cast<void*>(ptr)) U;
+    }
+
+    template<typename U, typename...Args>
+    void construct(U* ptr, Args&&... args) {
+        a_t::construct(static_cast<A&>(*this), ptr, std::forward<Args>(args)...);
+    }
+};
+
+using buffer_t = std::vector<uint8_t, default_init_allocator<uint8_t>>;
 
 static bool inline operator<(const KEY& a, const KEY& b) {
     if (a.obj_id < b.obj_id)
