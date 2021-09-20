186b60c5f901187e6d6d880bf90bbbb2d9c86ac2
Mark Harmstone <mark@harmstone.com>
2021-09-20 21:34:55 +0100

    add copyright notice to rollback.cpp

--- src/rollback.cpp
+++ src/rollback.cpp
@@ -1,3 +1,20 @@
+/* Copyright (c) Mark Harmstone 2021
+ *
+ * This file is part of ntfs2btrfs.
+ *
+ * Ntfs2btrfs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public Licence as published by
+ * the Free Software Foundation, either version 2 of the Licence, or
+ * (at your option) any later version.
+ *
+ * Ntfs2btrfs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public Licence
+ * along with Ntfs2btrfs. If not, see <https://www.gnu.org/licenses/>. */
+
 #include "ntfs2btrfs.h"
 #include "crc32c.h"
 #include <iostream>
