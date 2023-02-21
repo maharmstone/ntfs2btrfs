b4562950dc5e883508ab2d99db14d979f0a7c803
Mark Harmstone <mark@harmstone.com>
2023-02-21 23:27:07 +0000

    walk_btree: use concept rather than std::function

--- src/ntfs.cpp
+++ src/ntfs.cpp
@@ -465,7 +465,7 @@ string_view ntfs::find_sd(uint32_t id, ntfs_file& secure) {
 }
 
 static void walk_btree(const index_root& ir, const list<mapping>& mappings, const index_node_header& inh, ntfs& dev,
-                       const function<void(const index_entry&, string_view)>& func, unsigned int level) {
+                       const invocable<const index_entry&, string_view> auto& func, unsigned int level) {
     auto ent = reinterpret_cast<const index_entry*>((uint8_t*)&inh + inh.first_entry);
 
     do {
